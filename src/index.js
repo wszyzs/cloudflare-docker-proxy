2e83c9403b199c38df36f390f7b04122d315a291
wszyzs <98144681+wszyzs@users.noreply.github.com>
2025-03-15 23:24:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.wszy.online" + CUSTOM_DOMAIN]: dockerHub,
+  ["quay.wszy.online" + CUSTOM_DOMAIN]: "https://quay.io",
+  ["gcr.wszy.online" + CUSTOM_DOMAIN]: "https://gcr.io",
+  ["k8s-gcr.wszy.online" + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
+  ["k8s.wszy.online" + CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  ["ghcr.wszy.online" + CUSTOM_DOMAIN]: "https://ghcr.io",
+  ["cloudsmith.wszy.online" + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  ["ecr.wszy.online" + CUSTOM_DOMAIN]: "https://public.ecr.aws",
 
   // staging
   ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
